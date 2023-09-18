7fe88d8e1bf425ec14d0a00f32b96b0f6b3b9562
JCRoooD <juancarlos-99@live.com>
2023-09-18 15:28:49 -0400

    fixed betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,29 +6,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int tmp;
+	size_t i, j, min;
+	int tmp;
 
-    /* If array is NULL or size is less than 2, do nothing */
-    if (array == NULL || size < 2)
-        return;
-    /* Loop through the array and compare each element to the next */
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        /* If the element is greater than the next, swap them */
-        for (j = i + 1; j < size; j++)
-        {
-            /* Print the array after each swap */
-            if (array[j] < array[min])
-                min = j;
-        }
-        if (min != i) /* If the minimum is not the current element, swap */
-        {
-            tmp = array[i];
-            array[i] = array[min];
-            array[min] = tmp;
-            print_array(array, size);
-        }
-    }
+	/* If array is NULL or size is less than 2, do nothing */
+	if (array == NULL || size < 2)
+		return;
+	/* Loop through the array and compare each element to the next */
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		/* If the element is greater than the next, swap them */
+		for (j = i + 1; j < size; j++)
+		{
+			/* Print the array after each swap */
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i) /* If the minimum is not the current element, swap */
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,27 +6,27 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    size_t i, j, pivot;
-    int tmp;
+	size_t i, j, pivot;
+	int tmp;
 
-    /* If array is NULL or size is less than 2, do nothing */
-    if (array == NULL || size < 2)
-        return;
-    /* Loop through the array and compare each element to the next */
-    for (i = 0; i < size - 1; i++)
-    {
-        pivot = i;
-        /* If the element is greater than the next, swap them */
-        for (j = i + 1; j < size; j++)
-        {
-            /* Print the array after each swap */
-            if (array[j] < array[pivot])
-            {
-                tmp = array[pivot];
-                array[pivot] = array[j];
-                array[j] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	/* If array is NULL or size is less than 2, do nothing */
+	if (array == NULL || size < 2)
+		return;
+	/* Loop through the array and compare each element to the next */
+	for (i = 0; i < size - 1; i++)
+	{
+		pivot = i;
+		/* If the element is greater than the next, swap them */
+		for (j = i + 1; j < size; j++)
+		{
+			/* Print the array after each swap */
+			if (array[j] < array[pivot])
+			{
+				tmp = array[pivot];
+				array[pivot] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
